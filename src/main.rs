0ff19d9491ba09166ff96b92279d7fc27f09febb
Aleru <41040598+Aleru@users.noreply.github.com>
2019-04-16 03:26:42 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,35 @@ use std::fs::File;
 use std::io::BufReader;
 
 
-struct ArrAndCount{
+struct VecAndCount{
 	vec: Vec<i32>,
 	count: usize,
 }
 
-fn build_ArrAndCount(vec: Vec<i32>, count: usize)->ArrAndCount{
-ArrAndCount{
+// constructor
+fn build_VecAndCount(vec: Vec<i32>, count: usize)->VecAndCount{
+VecAndCount{
 	vec:vec,
 	count:count
 }
 }
 
-fn sort_and_count_inv(mut v1: Vec<i32>) -> ArrAndCount{
-	let mut n = v1.len();
+
+fn sort_and_count_inv(mut v1: Vec<i32>) -> VecAndCount{
+	let mut n = v1.len();    // number of elements in passed vec
 	
-	if n < 2 {
-		 build_ArrAndCount(v1,0)
+	if n < 2 {   					// base case returns the vec b.c already sorted and 0 because there has to be 
+									// 0 inversions
+		 build_VecAndCount(v1,0)
 	
 	}
 	else{
 		
-		let mut left_inv = sort_and_count_inv(v1[0..n/2].to_vec());
-		let mut right_inv = sort_and_count_inv(v1[n/2..n].to_vec());
-		let mut merged_inv = merge_and_count_split_inv(left_inv.vec,right_inv.vec);
+		let mut left_inv = sort_and_count_inv(v1[0..n/2].to_vec()); // recursively sort left half of the array
+		let mut right_inv = sort_and_count_inv(v1[n/2..n].to_vec());	// recursively right half of hte array
+		let mut merged_inv = merge_and_count_split_inv(left_inv.vec,right_inv.vec); // 
 
-		build_ArrAndCount(merged_inv.vec, merged_inv.count + left_inv.count+right_inv.count)
+		build_VecAndCount(merged_inv.vec, merged_inv.count + left_inv.count+right_inv.count)
 
 
 		
@@ -42,76 +45,80 @@ fn sort_and_count_inv(mut v1: Vec<i32>) -> ArrAndCount{
 	}
 
 
-
-fn merge_and_count_split_inv( v1:  Vec<i32>, v2: Vec<i32>) ->   ArrAndCount  {
-	let (mut i, mut j, mut split_inv) = (0,0,0);
-	let n = v1.len() * 2;
+//takes two sorted vectors, counts split inversions and returns 
+fn merge_and_count_split_inv( v1:  Vec<i32>, v2: Vec<i32>) ->   VecAndCount  {
+	let (mut l_ctr,mut r_ctr,mut split_inv) = (0,0,0);
 	let mut v3: Vec<i32> = Vec::new();
 
-	for _x in 0..n{
+	while l_ctr < v1.len() && r_ctr < v2.len(){
+		
+		if v1[l_ctr] < v2[r_ctr]{
+			v3.push(v1[l_ctr]);
+			l_ctr = l_ctr + 1;
+		}
+
+		else{
+			split_inv = split_inv + (v1.len() - l_ctr);
+			v3.push(v2[r_ctr]);
+			r_ctr = r_ctr + 1;
+
+		}}
+
+/*
+
+		WHEN THIS INVLADE CODE SUCH AS THE BELOW IS RAN COMPILER NEVER GETS TO THE RETURN STATEMENT AND IT COMPLAINS ABOUT NOT HAVING A RETURN VALUE INSTEAD OF THIS INVLAID CODE
+		v3.push(v1[l_ctr:v1.len()]); 
+
+
+*/
+
+
+		for _x in l_ctr.. v1.len(){
+			v3.push(v1[_x]);
+		}
+
+		for _x in r_ctr..v2.len(){
+			v3.push(v2[_x]);
+		}
+		build_VecAndCount(v3,split_inv)
+
 		
 
 
-		if v1[i] < v2[j] {
-			v3.push(v1[i]);
-			i = i + 1;
-			if i == v1.len() {
-			for _y in j.. n/2{
-				v3.push(v2[j]);
-				j = j + 1;
-			}
-			
-			break;
-
-		}
-			
-		}
-		else{
-			v3.push(v2[j]);
-			split_inv = split_inv + (n/2 - i);
-			j = j +1;
-			if j == v1.len() {
-
-			for _y in i.. n/2{
-			v3.push(v1[i]);
-			i = i + 1; }
-			break;
-
-		}
-		}
-		}
-
-	ArrAndCount {
-		vec: v3,
-		count: split_inv,
 	}
+	
 
 
-	}
 
 
 
 fn main() -> io::Result<()> {
-    let f = File::open("algo.txt")?;
-    let reader = BufReader::new(f);
 
-    let mut vec: Vec<i32> = Vec::new();
+	
+	let f = File::open("algo.txt")?;
 
-    for line in reader.lines() {
-    	let l = match line{
-    		Ok(str) => str,
-    		Err(error) => {
-    			panic!("there was an error parsing the line in the opened file {:?}", error)
-    		}
-    		};
-    	let input: i32 = l.trim().parse().expect("attempted to parse non numeric");
-       	 vec.push(input);
+	let reader = BufReader::new(f);
 
-    	}
+	let mut vec: Vec<i32> = Vec::new();
 
-    println!("{:?}", vec);
-          Ok(())
-    }
+	for line in reader.lines() {
+		let l = match line{
+			Ok(str) => str,
+			Err(error) => {
+				panic!("there was an error parsing the line in the opened file {:?}", error)
+			}
+			};
+		let input: i32 = l.trim().parse().expect("attempted to parse non numeric");
+		 vec.push(input);
+
+		};
+
+ 	 let vec_and_count_to_parse = sort_and_count_inv(vec);
+	println!("{}", vec_and_count_to_parse.count);
+			  Ok(())
+   
+
+ }
   
 
 
@@ -132,3 +139,4 @@ mod tests{
 }
 
 */
+
